2c69d5903c3156b73a18b92f230a7bce628f6db3
Chris <clj@ollure.com>
2019-01-06 01:00:01 -0600

    added function to animate on mouseover and stop at mouseout

--- scripts.js
+++ scripts.js
@@ -1,4 +1,28 @@
-function animateScript(){
-	document.getElementById("image").style.backgroundPosition = `-256px 0px`;
+var tID;
+
+function stopAnimate() {
+	clearInterval(tID);
+} // end stopAnimate function
+
+function animateScript() {
+	var position = 256;
+	const interval = 100;
+	const diff = 256;
+
+	tID = setInterval ( () => {
+
+
+	document.getElementById("image").style.backgroundPosition = `-${position}px 0px`;
+
+if (position < 1536)
+	{ position = position + diff;}
+ // increment position by 256each time
+ else 
+ { position = 256; }
+// reset position 
+
 }
+, interval ); // end of setInerval function
+
+} // end of animateScripr function
 
